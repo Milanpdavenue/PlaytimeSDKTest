5e6c5e9a9b4a2af89f66e0a6f05d93a215433d8c
Milanpdavenue <158189674+Milanpdavenue@users.noreply.github.com>
2024-04-09 16:24:59 +0530

    Delete playtimesdk directory

--- playtimesdk/src/main/java/com/playtime/sdk/listeners/OfferWallInitListener.java
+++
@@ -1,7 +0,0 @@
-package com.playtime.sdk.listeners;
-
-public interface OfferWallInitListener {
-    void onInitSuccess();
-    void onAlreadyInitializing();
-    void onInitFailed(String error);
-}
